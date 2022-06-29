5503e9dd099884400168aa58dfb99fbef1505d26
MBrian103 <oduorbrian103@gmail.com>
2022-06-29 03:46:28 -0700

    rectangle

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """class tectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle instance"""
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """Returns an informal of a Rectangle instance"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        rec_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rec_str += "#"
+            rec_str += "\n"
+        return rec_str[:-1]
+
+    def __repr__(self):
+        """Return a string representation of a Rectangle"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of a Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculates the perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
